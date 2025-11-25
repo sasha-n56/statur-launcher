6f8544ce68a4815bb3dc9d7970256f0ccd732c1f
alexandernab <naberezhnyy.an@gmail.com>
2025-11-25 21:45:58 +0100

    color import example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-stay-darko">Welcome to SvelteKit</h1>
+<div class="w-[200px] h-[200px] bg-stay-darko"></div>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
