89657b6bff400c16ff5cec4d109b7969fbc7b6a0
sasha-56n <sasha@iagon.com>
2025-11-29 11:23:58 +0100

    remove nesting by deleting LauncherText

--- src/components/LauncherWidget.svelte
+++ src/components/LauncherWidget.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { LAUNCHER_STEPS_DATA } from '../const';
-	import LauncherText from './LauncherText.svelte';
 	import WalletList from './WalletList.svelte';
 
 	let { title, label, description } = LAUNCHER_STEPS_DATA[0];
@@ -9,6 +8,10 @@
 <div
 	class=" flex max-w-[480px] flex-col items-center gap-8 rounded-4xl bg-base/5 p-6 shadow-[0_0_15px_0_rgba(78,42,80,0.08)]"
 >
-	<LauncherText {title} {label} {description} />
+	<div class="flex flex-col items-center">
+		<span class="text-lg leading-tight font-bold font-jakarta text-main-accent">{label}</span>
+		<h1 class="mb-4 text-[46px] leading-tight font-bold font-jakarta text-base">{title}</h1>
+		<p class="text-center text-grey-dark">{description}</p>
+	</div>
 	<WalletList />
 </div>
