e04e41ac03c438698a71a4e9b6d8c8943a32aaec
dzmitry <dzmitry@iagon.com>
2025-12-04 23:54:01 +0300

    add create-account and success pages

---
+++ src/routes/project-launcher/create-account-success/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import LauncherHeader from '$components/LauncherHeader.svelte';
+	import LinkButton from '$components/LinkButton.svelte';
+	import { t } from '$lib/i18n';
+</script>
+
+<div class="flex flex-col w-full items-center">
+	<LauncherHeader />
+	<main class="flex w-full h-full items-center justify-center max-w-md">
+		<div class="flex flex-col items-center">
+			<div>{$t('launcher.createAccountSuccessStep.label')}</div>
+			<div>{'Gotham City'}</div>
+			<div>{$t('launcher.createAccountSuccessStep.description')}</div>
+			<LinkButton href="/administration/metric-fields" name={$t('common.join')} />
+		</div>
+	</main>
+</div>
---
+++ src/routes/project-launcher/create-account/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import LauncherHeader from '$components/LauncherHeader.svelte';
+	import LinkButton from '$components/LinkButton.svelte';
+	import { t } from '$lib/i18n';
+</script>
+
+<div class="flex flex-col w-full items-center">
+	<LauncherHeader />
+	<main class="flex w-full h-full items-center justify-center max-w-md">
+		<div class="flex flex-col items-center">
+			<div>{$t('launcher.createAccountStep.label')}</div>
+			<div>{$t('launcher.createAccountStep.title')}</div>
+			<div>{$t('launcher.createAccountStep.description')}</div>
+			<LinkButton href="/project-launcher/create-account-success" name={$t('common.create')} />
+		</div>
+	</main>
+</div>
