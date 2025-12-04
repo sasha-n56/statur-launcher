aedd750188bcdb357fec93101a048352d05a875e
dzmitry <dzmitry@iagon.com>
2025-12-04 18:11:42 +0300

    add sign-in page

--- src/routes/sign-in/+page.svelte
+++ src/routes/sign-in/+page.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
 	import { t } from '$lib/i18n';
+	let wallet = $t('launcher.walletSignInStep.eternl');
 </script>
 
-<div>
-	<div>
+<div class="flex flex-col w-full items-center">
+	<header>
 		{$t('common.statur')}
 		{$t('common.projectLauncher')}
-	</div>
-	<main class="flex">
-		<div></div>
+	</header>
+	<main class="flex w-full h-full items-center justify-center max-w-md">
+		<div class="flex flex-col items-center">
+			<div class="">{$t('launcher.walletSignInStep.label')}</div>
+			<div>{$t('launcher.walletSignInStep.title')}</div>
+			<div>{$t('launcher.paymentPlanStep.description')}</div>
+			<button class="w-40 border">
+				<a href="/project-launcher/payment-plan" class=""
+					>{$t('launcher.walletSignInStep.actionButton', { values: { name: wallet } })}</a
+				>
+			</button>
+		</div>
 	</main>
 </div>
