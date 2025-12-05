9feddd7dd80494de82decd74197f74c1fb973023
dzmitry <dzmitry@iagon.com>
2025-12-05 11:21:52 +0300

    add metric-fields page

---
+++ src/components/NavigationSidebar.svelte
@@ -0,0 +1,16 @@
+<script>
+	const menuItems = [
+		{ label: 'Metric Fields', href: '/administration/metrics-fields' },
+		{ label: 'Dashboard Layout', href: '/administration/dashboard-layout' },
+		{ label: 'Profiles Layout', href: '/administration/profiles-layout' },
+		{ label: 'Badges', href: '/administration/badges' },
+		{ label: 'Administrators', href: '/administration/administrators' },
+		{ label: 'API & Payment', href: '/administration/api-and-payment' }
+	];
+</script>
+
+<nav class="flex flex-col border">
+	{#each menuItems as item}
+		<a href={item.href}> {item.label} </a>
+	{/each}
+</nav>
---
+++ src/routes/administration/metric-fields/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import LinkButton from '$components/LinkButton.svelte';
+	import SignUpHeader from '$components/SignUpHeader.svelte';
+	import { t } from '$lib/i18n';
+
+</script>
+<div class="flex flex-col w-full items-center">
+	<SignUpHeader />
+	<main class="flex w-full h-full items-center justify-center max-w-md">
+		<aside class="flex flex-col w-xs">
+			<div>Settings menu</div>
+      <NavigationS
+		</aside>
+		<div class="flex flex-col items-center">
+			<div>{$t('common.welcome')}</div>
+			<div>{$t('launcher.createAccountStep.title')}</div>
+			<LinkButton href="/sign-up/create-account-success" name={$t('common.create')} />
+		</div>
+	</main>
+</div>
