4ab0aaa90b2ddf55db070776cae1fa95c4c34cbc
dzmitry <dzmitry@iagon.com>
2025-12-09 16:46:41 +0300

    fix css variables fix project launcher page

--- src/routes/project-launcher/branding/+page.svelte
+++ src/routes/project-launcher/branding/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import LauncherText from '$lib/components/LauncherText.svelte';
 	import LinkButton from '$lib/components/LinkButton.svelte';
 	import { t } from '$lib/i18n';
 </script>
 
 <section class="flex w-full h-full items-center justify-center max-w-md">
 	<div class="flex flex-col items-center">
+    <LauncherText
 		<div>{$t('common.projectLauncher')}</div>
 		<div>{$t('launcher.brandingStep.title')}</div>
 		<div>{$t('launcher.brandingStep.description')}</div>
