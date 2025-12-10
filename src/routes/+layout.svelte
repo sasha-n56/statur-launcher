6609357c580ee9367c87ae4fea20c6a72e2951c2
dzmitry <dzmitry@iagon.com>
2025-12-10 11:30:01 +0300

    fix colors css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,6 @@
 	<link rel="icon" href={svelteFavicon} />
 </svelte:head>
 <!-- bg-l-MainAccent -->
-<div data-theme="light" class="min-h-screen w-full bg-[var(--l-STAY-Darko)] text-white">
+<div class="min-h-screen w-full bg-[var(--cl-STAY-Darko)] text-white">
 	{@render children()}
 </div>
--- src/routes/project-launcher/branding/+page.svelte
+++ src/routes/project-launcher/branding/+page.svelte
@@ -31,22 +31,22 @@
 	}
 
 	function onClick1() {
-		applyColorSet(PinkDark);
+		// applyColorSet(PinkDark);
 		// document.documentElement.style.setProperty('--l-MainAccent', '#555555');
-		// document.documentElement.setAttribute('data-theme', 'dark');
+		document.documentElement.setAttribute('data-brand', 'pink');
 	}
 
 	function onClick2() {
-		applyColorSet(GreenDark);
-		// document.documentElement.setAttribute('data-theme', 'light');
+		// applyColorSet(GreenDark);
+		document.documentElement.setAttribute('data-brand', 'green');
 	}
 </script>
 
 <section class="flex w-full h-full items-center justify-center max-w-md">
 	<div class="flex flex-col items-center">
 		<!-- <LauncherText -->
-		<div class="text-[var(--l-MainAccent)]">{$t('common.projectLauncher')}</div>
-		<div class="text-[var(--l-SecondAccent)]">{$t('launcher.brandingStep.title')}</div>
+		<div class="text-[var(--cl-MainAccent)]">{$t('common.projectLauncher')}</div>
+		<div class="text-[var(--cl-SecondAccent)]">{$t('launcher.brandingStep.title')}</div>
 		<div>{$t('launcher.brandingStep.description')}</div>
 		<LinkButton href="/project-launcher/create-account" name={$t('common.next')} />
 		<button onclick={onClick1}>color1</button>
