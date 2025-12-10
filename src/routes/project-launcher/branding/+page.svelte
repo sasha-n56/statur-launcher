b95e3b2e77027df4afb0933a4c30fcbb4e3eaeeb
dzmitry <dzmitry@iagon.com>
2025-12-10 12:28:48 +0300

    fix colors css

--- src/routes/project-launcher/branding/+page.svelte
+++ src/routes/project-launcher/branding/+page.svelte
@@ -3,33 +3,6 @@
 	import LinkButton from '$lib/components/LinkButton.svelte';
 	import { t } from '$lib/i18n';
 
-	const PinkDark = {
-		'--l-STAY-Darko': '#1F0726',
-		'--l-MainAccent': '#EB50AD',
-		'--l-MainAccent-20': '#EB50AD33',
-		'--l-MainAccent-light': '#FF59BD',
-		'--l-SecondAccent': '#4E2A50',
-		'--l-SecondAccent-08': '#4E2A5014',
-		'--l-SecondAccent-light': '#6E3A70'
-	};
-
-	const GreenDark = {
-		'--l-STAY-Darko': '#070E0C',
-		'--l-MainAccent': '#07A27B',
-		'--l-MainAccent-20': '#07A27B33',
-		'--l-MainAccent-light': '#09C193',
-		'--l-SecondAccent': '#1A4238',
-		'--l-SecondAccent-08': '#1A42380F',
-		'--l-SecondAccent-light': '#205044'
-	};
-
-	function applyColorSet(colors: any) {
-		const root = document.documentElement;
-		for (const key in colors) {
-			root.style.setProperty(key, colors[key]);
-		}
-	}
-
 	function onClick1() {
 		// applyColorSet(PinkDark);
 		// document.documentElement.style.setProperty('--l-MainAccent', '#555555');
